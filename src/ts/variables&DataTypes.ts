8631cc4515c437e4980d9069da65e58a5faf6a36
biplabCyberSwift <jobs.biplab.sharma@gmail.com>
2025-05-31 14:22:04 +0530

    first commit

---
+++ src/ts/variables&DataTypes.ts
@@ -0,0 +1,38 @@
+export const dataTypes = (): void => {
+    /**
+     * In JS/TS There are these data types listed below
+     * 
+     * Common Ones
+     *  number/Number
+     *  boolean/Boolean
+     *  string/String
+     *  object
+     *  Array
+     *  function
+     *  Date
+     *  Symbol
+     *  Bigint
+     * 
+     * Typescript Only
+     *  type
+     *  Record
+     *  enum
+     *  unknown
+     */
+
+    let stringType: string = "Biplab";
+    let stringType2: string = 'B';
+
+    let number1: number = 1.0;
+    let number2: number = 1;
+    let number3: number = 1.03;
+    let number4: number = 111111111111111111111.878668684554
+    let number5: bigint = 111111111111111111111111111111111n;
+
+    let status: boolean = true;
+    let status1: boolean = false;
+
+    
+
+    
+}
